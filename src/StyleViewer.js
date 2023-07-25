b52848a84df5db7ffab662e8adf1a93bcd2566eb
Wet Rain <98018258+Siyu1017@users.noreply.github.com>
2023-07-25 08:05:52 +0000

    Update

--- src/StyleViewer.js
+++ src/StyleViewer.js
@@ -10,7 +10,46 @@
     const StyleViewer = {
         target: document.body
     };
+
+    // 初始化
     StyleViewer.init = (target, config) => {
+        
+    }
+
+    // 取得所有樣式
+    StyleViewer.getAllStyle = (target) => {
 
     }
+
+    // 取得使用者代理樣式表樣式
+    StyleViewer.getDefaultStyle = (target) => {
+
+    }
+
+    // 取得陣列中的樣式
+    StyleViewer.getStyleByArray = (target, style) => {
+
+    }
+
+    // 取得字串中的樣式 ( 以空格分開 )
+    StyleViewer.getStyleByString = (target, style) => {
+
+    }
+
+    // 禁止 StyleViewer 對字串中的選擇器作用
+    StyleViewer.filterByString = (selectors) => {
+
+    }
+
+    // 禁止 StyleViewer 對陣列中的選擇器作用
+    StyleViewer.filterByArray = (selectors) => {
+
+    }
+
+    // 關閉 / 開啟 StyleViewer
+    StyleViewer.toggle = () => {
+
+    }
+
+    window.StyleViewer = StyleViewer;
 })()
