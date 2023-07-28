75f7fd6751dde7ce3e6d7c331e9d9a03ffb158c7
Wet Rain <98018258+Siyu1017@users.noreply.github.com>
2023-07-28 01:14:00 +0000

    Update

--- src/StyleViewer.js
+++ src/StyleViewer.js
@@ -7,6 +7,63 @@
 "use strict";
 
 (() => {
+    const hash = {
+        highlight: Hash(24, "ABCDEFGHIJKLMNOPQRSTUVWXYZ_-_abcdefghijklmnopqrstuvwxyz")
+    }
+
+    const StyleViewer = {
+        target: document.body,
+        selecting: false,
+        filters: ["StyleViewer", []],
+        highlightElement: Element("div", `svjs-${hash.highlight} svjs-highlight`, [["style", "display: none;"]]),
+    };
+
+    function GenerateStyle(s,v) {
+        var style = document.createElement("style");
+        style.innerHTML = `${s} { ${v} }`;
+        document.head.appendChild(style);
+    }
+
+    function Hash(n, c) { var c = c || 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789', r = '', l = c.length; for (let i = 0; i < n; i++) { r += c.charAt(Math.floor(Math.random() * l)); } return r; };
+
+    function Element(tagName, classList, attributes) {
+        var element = document.createElement(tagName);
+        typeof classList === "array" ? classList.forEach(c => {
+            element.classList.add(c);
+        }) : typeof classList === "string" ? classList.split(" ").forEach(c => {
+            element.classList.add(c);
+        }) : null;
+        typeof attributes === "array" ? attributes.forEach(a => {
+            element.setAttribute(a[0], a[1]);
+        }) : null;
+        return document.createElement(element);
+    }
+
+    function getPosition(element) {
+        function offset(el) {
+            var rect = el.getBoundingClientRect(),
+                scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+                scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+            return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+        }
+        return { x: offset(element).left, y: offset(element).top };
+    }
+
+    function highlightElement(element) {
+        var position = element.nodeName.toLowerCase() == "path" ? {
+            x: element.getClientRects()[0].left,
+            y: element.getClientRects()[0].top
+        } : getPosition(element);
+        var size = {
+            width: element.nodeName.toLowerCase() == "path" ? element.getClientRects()[0].width : element.scrollWidth == 0 ? element.offsetWidth : element.scrollWidth,
+            height: element.nodeName.toLowerCase() == "path" ? element.getClientRects()[0].height : element.scrollHeight == 0 ? element.offsetHeight : element.scrollHeight
+        }
+        StyleViewer.highlightElement.width = size.width + "px";
+        StyleViewer.highlightElement.height = size.height + "px";
+        StyleViewer.style.top = position.y + "px";
+        StyleViewer.style.left = position.x + "px";
+    }
+
     function getStyle(a, c) {
         var b; if ((b = (a.ownerDocument || document).defaultView) && b.getComputedStyle) return c = c.replace(/([A-Z])/g, "-$1").toLowerCase(), b.getComputedStyle(a, null).getPropertyValue(c); if (a.currentStyle) return c = c.replace(/\-(\w)/g, function (d, e) { return e.toUpperCase() }), b = a.currentStyle[c], /^\d+(em|pt|%|ex)?$/i.test(b) ? function (d) { var e = a.style.left, f = a.runtimeStyle.left; a.runtimeStyle.left = a.currentStyle.left; a.style.left = d || 0; d = a.style.pixelLeft + "px"; a.style.left = e; a.runtimeStyle.left = f; return d; }(b) : b;
     };
@@ -81,12 +138,6 @@
         }
         return RS;
     }
-
-    const StyleViewer = {
-        target: document.body,
-        selecting: false,
-        filters: ["StyleViewer", []]
-    };
 
     // 初始化
     StyleViewer.init = (target) => {
